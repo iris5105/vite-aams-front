bd194cf10ecedab6f0cec9f73c1e83b9bec29d6c
iris5105 <iris510593@gmail.com>
2025-03-14 16:43:52 +0900

    panel 높이 조절 후 불피룡한 코드 삭제

--- vite-aams-front/src/temp/SeperatePage_TB.jsx
+++ vite-aams-front/src/temp/SeperatePage_TB.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Splitter,  Typography } from 'antd'
 const { Text } = Typography;
 
 const SeperatePage_TB = ({prop, children = [], onSizeChange}) => {
   const [ topChild, bottomChild] = children;
-  // const bottomPanelRef = useRef(null);
-
   const handleResize = (newSizes) => {
-    if (onSizeChange) {
       onSizeChange(newSizes); // 부모 컴포넌트로 크기 변경 값 전달
-    }
   };
+
 
   return (
     <Splitter layout='vertical' onResize={handleResize}>
-      <Splitter.Panel defaultSize={'50%'} min='100' style={{ overflow: 'hidden' }} >
+      <Splitter.Panel className="topPanel" defaultSize={'50%'} min='100' style={{ overflow: 'hidden' }} >
           {topChild || <Text>Top</Text>}
       </Splitter.Panel>
       <Splitter.Panel
-          id='BottomPanel'
+          className="bottomPanel"
           defaultSize={'50%'}
           style={{ overflow: 'hidden' }}
           min='100'
